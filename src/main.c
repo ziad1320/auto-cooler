a81907438442e603080794feb6785b66cefac69d
ziad <zramdan311@gmail.com>
2026-06-30 14:43:39 +0300

    Add USART logging functionality for ADC data and temperature

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include "../Lcd/Lcd.h"
 #include "../Pwm/Pwm.h"
 #include "../Timer/Timer.h"
+#include "../Usart/Usart.h"
 #include "../App/StateMachine.h"
 
 /* --- Hardware Mapping Definitions --- */
@@ -53,6 +54,40 @@ void IntToTempString(uint16 rawAdc, char* str) {
     str[7] = '\0';
 }
 
+/* Helper to convert raw integer to string for UART logging safely */
+void IntToString(uint16 num, char* str) {
+    int i = 0;
+    if (num == 0) {
+        str[i++] = '0';
+        str[i] = '\0';
+        return;
+    }
+    char temp[6];
+    int temp_idx = 0;
+    while (num > 0) {
+        temp[temp_idx++] = (num % 10) + '0';
+        num /= 10;
+    }
+    while (temp_idx > 0) {
+        str[i++] = temp[--temp_idx];
+    }
+    str[i] = '\0';
+}
+
+/* helper to format and send a CSV log over USB */
+void LogDataToPC(uint16 rawAdc, const char* tempString) {
+    char rawStr[6];
+
+    /* Safely format just the raw integer without overflowing the buffer */
+    IntToString(rawAdc, rawStr);
+
+    // Print in CSV format: [Raw_ADC], [Formatted_Temp]
+    Usart2_TransmitString(rawStr);
+    Usart2_TransmitString(", ");
+    Usart2_TransmitString(tempString);
+    Usart2_TransmitString("\r\n"); // Carriage Return + Line Feed
+}
+
 int main(void) {
     char tempString[10];
     uint16 lastDisplayedRaw = 0xFFFF; /* Track RAW value to catch every 0.1 decimal change */
@@ -64,6 +99,9 @@ int main(void) {
     Rcc_Enable(RCC_GPIOD); /* LCD Port */
     Rcc_Enable(RCC_ADC1);
     Rcc_Enable(RCC_TIM2);  /* PWM Port */
+
+    /* ADDED: Enable USART2 Clock */
+    Rcc_Enable(RCC_USART2);
 
     /* 2. Init LCD */
     Lcd_Init();
@@ -87,6 +125,10 @@ int main(void) {
     /* 7. Init PWM */
     Pwm_Init(TIMER_2, PWM_CHANNEL_2, 15, 999);
     Pwm_Start(TIMER_2, PWM_CHANNEL_2);
+
+    /* ADDED: Init USART2 and Print the CSV Header */
+    Usart2_Init();
+    Usart2_TransmitString("Raw_ADC, Temperature\r\n");
 
     /* Allow ADC analog circuits to stabilize */
     for(volatile int i = 0; i < 5000; i++) { __asm("NOP"); }
@@ -112,6 +154,9 @@ int main(void) {
 
             /* * Update Fan, PWM, Alarm LED, and Bottom Row of LCD */
             StateMachine_Update(displayTemp);
+
+            /* ADDED: Log the data to the PC whenever a change happens */
+            LogDataToPC(displayRaw, tempString);
 
             lastDisplayedRaw = displayRaw;
         }
